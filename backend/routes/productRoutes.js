3012365787c902e38c1e0dd244fc400351e641d8
Anna Phuong Anh <phuong.nhu@exozet.com>
2020-10-23 08:50:58 +0200

    Error Handling

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,33 +1,35 @@
 import express from "express";
 import asyncHandler from "express-async-handler";
+const router = express.Router();
 import Product from "../models/productModel.js";
 
-const router = express.Router();
-// @desc Fetch all products
-// @route Get /api/products
-// @access Public
+// @desc    Fetch all products
+// @route   GET /api/products
+// @access  Public
 router.get(
   "/",
   asyncHandler(async (req, res) => {
     const products = await Product.find({});
-    res.json(products)
+
+    res.json(products);
   })
 );
 
-
-// @desc Fetch single product
-// @route Get /api/products/:id
-// @access Public
+// @desc    Fetch single product
+// @route   GET /api/products/:id
+// @access  Public
 router.get(
   "/:id",
   asyncHandler(async (req, res) => {
     const product = await Product.findById(req.params.id);
+
     if (product) {
       res.json(product);
     } else {
-      res.status(404).json({ message: "Prouduct not found" });
+      res.status(404)
+      throw new Error('Product not found');
     }
-    res.json(product);
   })
 );
+
 export default router;
