56896c5e023b256b522aaba538f7ff6c988a1d14
Anna Phuong Anh <phuong.nhu@exozet.com>
2020-10-26 10:58:54 +0100

    store to screen

---
+++ frontend/src/store.js
@@ -0,0 +1,17 @@
+import {createStore, combineReducers, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import {productListReducer} from './reducers/productReducers'
+
+
+const reducer = combineReducers({
+    productList: productListReducer
+})
+
+const initialSate = {} 
+
+
+const middleware = [thunk ]
+const store = createStore(reducer, initialSate, composeWithDevTools(applyMiddleware(...middleware)))
+
+export default store
