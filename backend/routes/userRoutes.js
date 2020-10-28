8e739b56a7a0db4146e6dc2d376d04205a7f8d1d
Anna Phuong Anh <phuong.nhu@exozet.com>
2020-10-28 09:11:34 +0100

    BE user authntication

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,17 @@
+import express from "express";
+
+import {
+  authUser,
+  getUserProfile,
+  registerUser,
+} from "../controllers/userControllers.js";
+import { protect } from "../middlerware/authMiddlerware.js";
+const router = express.Router();
+
+router.post("/login", authUser);
+
+router.route("/").post(registerUser);
+
+router.route("/profile").get(protect, getUserProfile);
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -6,11 +6,14 @@ import connectDB from "./config/db.js";
 import {notFound, errorHandler } from './middlerware/errorMiddlerware.js'
 
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 
 dotenv.config();
 connectDB();
 
 const app = express();
+
+app.use(express.json());
 
 app.use((req, res, next) => {
   console.log(req.originalUrl);
@@ -22,6 +25,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
 app.use(notFound);
 
